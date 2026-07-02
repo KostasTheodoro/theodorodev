f1b9a29843e21b43e245344b93f59a74ddd0e069
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2026-07-03 02:17:11 +0300

    typed content query helpers

---
+++ src/lib/content.ts
@@ -0,0 +1,28 @@
+import { getCollection, type CollectionEntry } from 'astro:content';
+
+type Lang = CollectionEntry<'projects'>['data']['lang'];
+
+function byOrder(a: CollectionEntry<'projects'>, b: CollectionEntry<'projects'>) {
+  return a.data.order - b.data.order;
+}
+
+export async function getProjects(lang: Lang) {
+  const entries = await getCollection('projects', ({ data }) => data.lang === lang);
+  return entries.sort(byOrder);
+}
+
+export async function getFeaturedProjects(lang: Lang) {
+  const entries = await getCollection(
+    'projects',
+    ({ data }) => data.lang === lang && data.featured,
+  );
+  return entries.sort(byOrder);
+}
+
+export async function getProjectBySlug(slug: string, lang: Lang) {
+  const entries = await getCollection(
+    'projects',
+    ({ data }) => data.slug === slug && data.lang === lang,
+  );
+  return entries[0];
+}
