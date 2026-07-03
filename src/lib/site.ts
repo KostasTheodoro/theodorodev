9d8ebabce69268df2abcd3875b28adcb032c1aee
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2026-07-03 06:35:56 +0300

    centralize real contact details in a shared site config

---
+++ src/lib/site.ts
@@ -0,0 +1,10 @@
+export const siteContact = {
+  email: 'k.m.theodoropoulos@gmail.com',
+  emailHref: 'mailto:k.m.theodoropoulos@gmail.com',
+  phoneDisplay: '+30 697 494 6459',
+  phoneHref: 'tel:+306974946459',
+  whatsappHref: 'https://wa.me/306974946459',
+  githubHref: 'https://github.com/KostasTheodoro',
+  // TODO: connect manually later
+  linkedinHref: '#',
+} as const;
