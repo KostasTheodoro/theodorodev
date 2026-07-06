f3e5ab1664666d097c5abfad9fccb9299303ef6b
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2026-07-06 04:11:15 +0300

    email change

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,6 @@
 export const siteContact = {
-  email: "k.m.theodoropoulos@gmail.com",
-  emailHref: "mailto:k.m.theodoropoulos@gmail.com",
+  email: "kostas@theodorodev.com",
+  emailHref: "mailto:kostas@theodorodev.com",
   phoneDisplay: "+30 697 494 6459",
   phoneHref: "tel:+306974946459",
   whatsappHref: "https://wa.me/306974946459",
